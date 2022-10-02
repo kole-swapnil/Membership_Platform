35c525a607eb2ec693e88f03e1e4bdc74e3909b2
alsha <alshabehera7@gmail.com>
2022-10-03 02:50:48 +0530

    Footer completed

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Blockcomp from './components/BlockComp/Blockcomp';
+import Footer from './components/Footer/Footer'
 import React from 'react';
 
 function Homepage() {
@@ -12,6 +13,7 @@ function Homepage() {
       </div>
       <span className="v133_304">How The Runway Helps You</span>
       <div id="container"></div>
+      <Footer/>
       <a className="copyRight" href="#">@2022 by The Runway Inc.</a>
       
       
