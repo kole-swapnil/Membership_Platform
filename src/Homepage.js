c7e7ea3f1fdb9e4fb0ae1f152ec96fc7ad6e1666
prachinayak1509 <prachinayak1509@gmail.com>
2022-10-03 01:00:11 +0530

    Carousel

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,6 +4,9 @@ import Cards from './components/Cards/Cards';
 import CardsNFT from './components/CardsNFT/CardsNFT';
 import React from 'react';
 
+import Carousel from './components/Carousel/Carousel';
+import { CarouselData } from './components/Carousel/CarouselData';
+
 
 function Homepage() {
   return (
@@ -13,6 +16,7 @@ function Homepage() {
       </div>
       <span className="v133_304">How The Runway Helps You</span>
       <div id="container"><Cards/>
+      <Carousel slides={CarouselData} />
       <CardsNFT/>
       </div>
       <a className="copyRight" href="#">@2022 by The Runway Inc.</a>
